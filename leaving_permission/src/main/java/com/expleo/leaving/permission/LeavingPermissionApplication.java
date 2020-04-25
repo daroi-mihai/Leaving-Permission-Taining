a7f7789ecac4df33200e6f82ab3d8149333d8a42
Daroi Mihai <daroi.mihai@gmail.com>
2020-04-25 09:05:31 +0300

    Initial commit.

---
+++ leaving_permission/src/main/java/com/expleo/leaving/permission/LeavingPermissionApplication.java
@@ -0,0 +1,14 @@
+package com.expleo.leaving.permission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavingPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavingPermissionApplication.class, args);
+		System.out.println("Hello World!!!");
+	}
+
+}
---
+++ leaving_permission/src/test/java/com/expleo/leaving/permission/LeavingPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expleo.leaving.permission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavingPermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
